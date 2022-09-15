03763c406966b425103bc52f3a9369bb4b46541d
Osman Dağdeviren <my@iosman.us>
2022-09-15 04:58:11 +0300

    v0.1

--- ping.sh
+++ ping.sh
@@ -10,13 +10,13 @@ iplist=$(cat $1)
 
 if [ -z ${2+x} ]; then times=10; else times=$2; fi
 
-echo -e 'IP Address\tPING' >> result_$1
-echo -e 'IP Address\t\tPING'
+echo -e 'IP Address\tPING' >> result_$1 # For record
+echo -e 'IP Address\t\tPING'            # For CLI
 
-for f in $FILES
+for ip in $iplist
 do
-  preping=$(ping -c 2 $f)
-  ping=$(ping -c $times $f | tail -1 | awk '{print $4}' | cut -d '/' -f 2)
-  echo -e $f'\t'$ping >> result_$1
-  echo -e $f'\t\t'$ping
+  preping=$(ping -c 2 $ip)
+  ping=$(ping -c $times $ip | tail -1 | awk '{print $4}' | cut -d '/' -f 2)
+  echo -e $ip'\t'$ping >> result_$1
+  echo -e $ip'\t\t'$ping
 done
